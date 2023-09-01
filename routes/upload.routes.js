59a699957294f9eb6782c74b2fa8069567d789be
albertocomb <combattialberto@gmail.com>
2023-09-01 09:40:39 +0200

    alberto

--- routes/upload.routes.js
+++ routes/upload.routes.js
@@ -1,8 +1,8 @@
 const router = require("express").Router()
 
-const uploaderMiddleware = require("../Middleware/uploader.middleware")
+const uploaderMiddleware = require("../controllers/uploader.middleware")
 
-router.post('/image', uploaderMiddleware.single('avatar'), (req, res) => {
+router.post('/image', uploaderMiddleware.single('imageData'), (req, res) => {
 
     if (!req.file) {
         res.status(500).json({ errorMessage: 'Error cargando el archivo' })
